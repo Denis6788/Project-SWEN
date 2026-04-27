74e12a38faabc63a4a38e9a36057e25e3a592556
gabriel6964 <144431250+gabriel6964@users.noreply.github.com>
2026-04-27 20:19:14 +0200

    adding the updated version of app controller

--- phase2/src/phase2 java version1/AppController.java
+++ phase2/src/phase2 java version1/AppController.java
@@ -1,43 +1,92 @@
 import java.time.LocalDate;
-import java.util.*;
 
 public class AppController {
 
-    private Map<String, Food> foods = new HashMap<>();
-    private Map<String, Exercise> exercises = new HashMap<>();
-    private Log log = new Log(LocalDate.now());
+    private final FoodCollection foodCollection;
+    private final LogManager logManager;
+    private final ExerciseCollection exerciseCollection; // NEW
+    private LocalDate currentDate;
 
-    public Log getLog() {
-        return log;
+    public AppController(FoodCollection foodCollection, LogManager logManager, ExerciseCollection ec) {
+        this.foodCollection = foodCollection;
+        this.logManager = logManager;
+        this.exerciseCollection = ec; // NEW
+        this.currentDate = LocalDate.now();
+
+        logManager.getOrCreate(currentDate);
     }
 
-   
-
-    public void createFood(String name, double calories, double fat, double carbs, double protein) {
-        foods.put(name, FoodFactory.createBasic(name, calories, fat, carbs, protein));
+    public void setDate(LocalDate date) {
+        this.currentDate = date;
+        logManager.getOrCreate(date);
     }
 
-    
+    public void addFood(String name, double servings) {
+        FoodItem food = foodCollection.findFood(name);
 
-    public void createExercise(String name, double burnRate) {
-        exercises.put(name, new Exercise(name, burnRate));
-    }
-
-   
-
-    public void addFoodToLog(String name, double amount) {
-        Food food = foods.get(name);
-
-        if (food != null) {
-            log.addFood(new FoodEntry(food, amount));
+        if (food == null) {
+            System.out.println("Food not found!");
+            return;
         }
+
+        logManager.getOrCreate(currentDate).addEntry(new LogEntry(food, servings));
+        System.out.println("Added food: " + name);
     }
 
-    public void addExerciseToLog(String name, double minutes) {
-        Exercise exercise = exercises.get(name);
+    // NEW
+    public void addExercise(String name, double minutes) {
+        Exercise ex = exerciseCollection.find(name);
 
-        if (exercise != null) {
-            log.addExercise(new ExerciseEntry(exercise, minutes));
+        if (ex == null) {
+            System.out.println("Exercise not found!");
+            return;
         }
+
+        logManager.getOrCreate(currentDate).addExercise(new ExerciseEntry(ex, minutes));
+        System.out.println("Added exercise: " + name);
+    }
+
+    public void showSummary() {
+        DailyLog log = logManager.getOrCreate(currentDate);
+
+        System.out.println("\n=== DATE: " + currentDate + " ===");
+
+        System.out.println("\n--- FOODS ---");
+        for (LogEntry e : log.getEntries()) {
+            System.out.println(e);
+        }
+
+        System.out.println("\n--- EXERCISES ---"); // NEW
+        for (ExerciseEntry e : log.getExercises()) {
+            System.out.println(e);
+        }
+
+        double consumed = log.getTotalCalories();
+        double burned = log.getBurnedCalories(); // NEW
+        double net = log.getNetCalories();       // NEW
+
+        System.out.println("\nConsumed: " + consumed);
+        System.out.println("Burned: " + burned);
+        System.out.println("Net: " + net);
+
+        System.out.println("Goal: " + log.getCalorieLimit());
+
+        double diff = log.getCalorieLimit() - net; // NEW
+        System.out.println("Remaining: " + diff);
+
+        System.out.println("Weight: " + log.getWeight());
+
+        Nutrients n = log.getTotalNutrients();
+        System.out.println("Fat: " + n.fatPercent() + "%");
+        System.out.println("Carbs: " + n.carbPercent() + "%");
+        System.out.println("Protein: " + n.proteinPercent() + "%");
+    }
+
+    public void save() throws Exception {
+        foodCollection.save("foods.csv");
+        exerciseCollection.save("exercise.csv"); // NEW
+        logManager.save("log.csv");
+
+        System.out.println("Saved successfully!");
     }
 }
